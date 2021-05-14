a80b8bbdeb37aea53043b0d63088e46c47f30392
pedro-hdias <83011148+pedro-hdias@users.noreply.github.com>
2021-05-14 09:56:47 -0300

    Incersão de script de data/hora.

--- assets/scripts/data.js
+++ assets/scripts/data.js
@@ -1,26 +1,51 @@
+var Elem = document.getElementById('Clock');
+
 var now = new Date();
-var mName = now.getMonth() +1 ; 
-var dName = now.getDay() +1; 
-var dayNr = now.getDate(); 
-var yearNr=now.getYear(); 
-if(dName==1) {Day = "Domingo";} 
-if(dName==2) {Day = "Segunda-feira";} 
-if(dName==3) {Day = "Ter&#231;a-feira";} 
-if(dName==4) {Day = "Quarta-feira";} 
-if(dName==5) {Day = "Quinta-feira";} 
-if(dName==6) {Day = "Sexta-feira";} 
-if(dName==7) {Day = "S&aacute;bado";} 
-if(mName==1){Month = "01";} 
-if(mName==2){Month = "02";} 
-if(mName==3){Month = "03";} 
-if(mName==4){Month = "04";} 
-if(mName==5){Month = "05";} 
-if(mName==6){Month = "06";} 
-if(mName==7){Month = "07";} 
-if(mName==8){Month = "08";} 
-if(mName==9){Month = "09";} 
-if(mName==10){Month = "10";} 
-if(mName==11){Month = "11";} 
-if(mName==12){Month = "12";} 
-if(yearNr < 2000) {Year = 1900 + yearNr;} else {Year = yearNr;} 
-var todaysDate =(" " + Day + " " + dayNr + "/" + Month +  "/" + Year); 
+var mName = now.getMonth() + 1;
+var dName = now.getDay() + 1;
+var dayNr = now.getDate();
+var yearNr = now.getYear();
+if (dName == 1) {
+    Day = "Domingo";
+}
+if (dName == 2) {
+    Day = "Segunda-feira";
+}
+if (dName == 3) {
+    Day = "Ter&#231;a-feira";
+}
+if (dName == 4) {
+    Day = "Quarta-feira";
+}
+if (dName == 5) {
+    Day = "Quinta-feira";
+}
+if (dName == 6) {
+    Day = "Sexta-feira";
+}
+if (dName == 7) {
+    Day = "S&aacute;bado";
+}
+if (!mName < 10) {
+    Month = "0" + mName;
+}
+yearNr = 1900 + yearNr;
+var data = Day + "/" + Month + "/" + yearNr;
+
+function Horario() {
+    var Hoje = new Date();
+    var Horas = Hoje.getHours();
+    if (Horas < 10) {
+        Horas = "0" + Horas;
+    }
+    var Minutos = Hoje.getMinutes();
+    if (Minutos < 10) {
+        Minutos = "0" + Minutos;
+    }
+    var Segundos = Hoje.getSeconds();
+    if (Segundos < 10) {
+        Segundos = "0" + Segundos;
+    }
+    Elem.innerHTML = `Olá, hoje é: ${data}, ${Horas}:${Minutos}:${Segundos}`;
+}
+window.setInterval("Horario()", 1000);
